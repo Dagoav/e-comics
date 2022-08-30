4fa7ae8fb27ddcf56ab7af0638ae5567528cae54
dagoav <dagoav89@gmail.com>
2022-08-29 23:05:29 -0500

    redux, react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from './pages/Home.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
