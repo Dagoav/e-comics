f9b47709c4e97797d8a09fe67a8ef18e2def446c
dagoav <dagoav89@gmail.com>
2022-08-31 16:36:52 -0500

    fix carrousel styles, images

--- src/components/carrousel/Carrousel2.jsx
+++ src/components/carrousel/Carrousel2.jsx
@@ -3,13 +3,15 @@ import Carousel from 'react-bootstrap/Carousel';
 import hero1 from "../../assets/intro1.jpg"
 import hero2 from "../../assets/doomsday.jpg"
 import hero3 from "../../assets/comic_hand.jpg"
+import Item from './Item';
 import "./Carrousel.css"
 
 const ControlledCarousel = () => {
     const [index, setIndex] = useState(0);
-    const listImages = [hero1, hero2, hero3]
-    let x = 800
-    let y = 350
+    const listImg_item1 = [hero1, hero2, hero3]
+    const listImg_item2 = [hero2, hero1, hero3]
+    const listImg_item3 = [hero1, hero2]
+    const listImg_item4 = [hero1]
 
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
@@ -17,90 +19,23 @@ const ControlledCarousel = () => {
 
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <div className="images-box">
-                    {
-                        listImages.map((hero, i) => (
-                            <img
-                                key={`hero-${i}`}
-                                className="d-block w-100"
-                                src={hero1}
-                                alt="First slide"
-                                width={x}
-                                height={y}
-                            />
-                        ))
-                    }
-
-                </div>
-                {/* <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption> */}
+            <Carousel.Item >
+                <Item listImages={listImg_item1} >
+                </Item>
             </Carousel.Item>
-            <Carousel.Item>
-                <div className="images-box">
-                    <img
-                        className="d-block w-100"
-                        src={hero3}
-                        alt="First slide"
-                        width={x}
-                        height={y}
-                    />
-                    <img
-                        className="d-block w-100"
-                        src={hero2}
-                        alt="First slide"
-                        width={x}
-                        height={y}
-                    />
-                    <img
-                        className="d-block w-100"
-                        src={hero1}
-                        alt="First slide"
-                        width={x}
-                        height={y}
-                    />
-                </div>
-
-                {/* <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption> */}
+            <Carousel.Item >
+                <Item listImages={listImg_item2} >
+                </Item>
             </Carousel.Item>
-            <Carousel.Item>
-                <div className="images-box">
-                    <img
-                        className="d-block w-100"
-                        src={hero1}
-                        alt="First slide"
-                        width={x}
-                        height={y}
-                    />
-                    <img
-                        className="d-block w-100"
-                        src={hero2}
-                        alt="First slide"
-                        width={x}
-                        height={y}
-                    />
-                    <img
-                        className="d-block w-100"
-                        src={hero3}
-                        alt="First slide"
-                        width={x}
-                        height={y}
-                    />
-                </div>
-
-                {/* <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption> */}
+            <Carousel.Item >
+                <Item listImages={listImg_item3} >
+                </Item>
             </Carousel.Item>
-        </Carousel>
+            <Carousel.Item >
+                <Item listImages={listImg_item4} >
+                </Item>
+            </Carousel.Item>
+        </Carousel >
     );
 }
 
