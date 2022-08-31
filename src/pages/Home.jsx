b646aa7df2b4c544b27e9abbffca80416347653b
SergioMoyano <smoyano07@hotmail.com.ar>
2022-08-31 00:30:59 -0300

    Sidebar css changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Carrousel2 from "../components/carrousel/Carrousel2";
 import Navbar2 from "../components/navBar/Navbar2";
+import Sidebar from "../components/sidebar/Sidebar";
 // import Navbar from "../components/navBar/Navbar2";
 import "./Home.css"
 
@@ -15,6 +16,7 @@ const Home = () => {
             <div className="row">
                 <main>
                     <Carrousel2 />
+                    <Sidebar />
                 </main>
             </div>
             <div className="row">
