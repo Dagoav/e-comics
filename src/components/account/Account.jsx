283390ceeaa09af20a2ce65621276718bf08a38b
dagoav <dagoav89@gmail.com>
2022-08-31 14:35:24 -0500

    account

---
+++ src/components/account/Account.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./Account.css"
+
+const Account = () => {
+    return (
+        <NavDropdown className='mx-2' title="Cuenta" id="navbarScrollingDropdown">
+            <div className='me-4'>
+                <NavDropdown.Item href="#action4" className='d-flex'>
+                    <span className="material-symbols-outlined" >
+                        account_circle
+                    </span>
+                    <span className='ms-2'>
+                        Login
+                    </span>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action5" className='d-flex'>
+                    <span className="material-symbols-outlined">
+                        book
+                    </span>
+                    <span className='ms-2'>
+                        Favoritos
+                    </span>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action6" className='d-flex'>
+                    <span className="material-symbols-outlined">
+                        upload_file
+                    </span>
+                    <span className='ms-2'>
+                        Subir Comic
+                    </span>
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action7" className='d-flex'>
+                    <span className="material-symbols-outlined">
+                        logout
+                    </span>
+                    <span className='ms-2'>
+                        Salir
+                    </span>
+                </NavDropdown.Item>
+            </div>
+        </NavDropdown>
+    )
+}
+
+export default Account;
