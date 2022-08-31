e19e281552389a06793dfe76b5974b74cd6c0d83
dagoav <dagoav89@gmail.com>
2022-08-31 14:35:46 -0500

    redux dark mode

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,106 +1,20 @@
-import dotenv from "dotenv"
-dotenv.config()
+// import dotenv from "dotenv"
+// dotenv.config("../.env")
 
-const backendURL = process.env.REACT_APP_API || "http://127.0.0.1:3001";
+const backendURL = "http://127.0.0.1:3001";
+// const backendURL = process.env.REACT_APP_API || "http://127.0.0.1:3001";
 
-export const getAllCountries = () => dispatch => {
-    return fetch(`${backendURL}/countries`)
+export const getAllComics = () => dispatch => {
+    return fetch(`${backendURL}/comics`)
         .then(response => response.json())
-        .then(countries => {
-            dispatch({ type: "GET_ALL_COUNTRIES", payload: countries })
+        .then(comics => {
+            dispatch({ type: "GET_ALL_COMICS", payload: comics })
         })
 };
 
-export const getCountriesByname = (name) => dispatch => {
-    return fetch(`${backendURL}/countries/search?name=${name}`)
-        .then(response => response.json())
-        .then(countries => {
-            dispatch({ type: "GET_COUNTRIES_BYNAME", payload: countries })
-        })
-        .catch((response) => console.log(response))
-};
-
-export const getCountryDetail = (id) => dispatch => {
-    return fetch(`${backendURL}/countries/${id}`)
-        .then(response => response.json())
-        .then(country => {
-            dispatch({ type: "GET_COUNTRY_DETAIL", payload: country })
-        })
-};
-
-
-export const getActivities = () => dispatch => {
-    return fetch(`${backendURL}/activities`)
-        .then(response => response.json())
-        .then(activities => {
-            dispatch({ type: "GET_ALL_ACTIVITIES", payload: activities })
-        })
-};
-
-export const getContinents = () => dispatch => {
-    return fetch(`${backendURL}/countries/continents`)
-        .then(response => response.json())
-        .then(continents => {
-            dispatch({ type: "GET_ALL_CONTINENTS", payload: continents })
-        })
-};
-
-// Create activity
-export const postActivity = (_body) => dispatch => {
-    return fetch(`${backendURL}/activities/byCountry`,
-        {
-            method: "POST",
-            body: JSON.stringify(_body),
-            headers: { "Content-type": "application/json; charset=UTF-8" }
-        })
-        .then(response => response.json())
-        .then(newActivity => {
-            if (newActivity) {
-                dispatch({ type: "POST_ACTIVITY", payload: newActivity })
-
-            }
-            return newActivity;
-        })
-
-};
-
-
-// filters
-export const orderByContinents = (continent) => dispatch => {
-    return fetch(`${backendURL}/countries/byContinent?name=${continent}`)
-        .then(response => response.json())
-        .then(continents => {
-            dispatch({ type: "ORDER_BY_CONTINENTS", payload: continents })
-        })
-}
-
-export const orderByActivity = (activity) => dispatch => {
-    return fetch(`${backendURL}/countries/byActivity?name=${activity}`)
-        .then(response => response.json())
-        .then(countries => {
-            dispatch({ type: "ORDER_BY_ACTIVITY", payload: countries })
-        })
-}
-
-
-export const orderByname = (order) => {
+export const setTheme = (theme) => {
     return {
-        type: "ORDER_BY_NAME",
-        payload: order
-    }
-}
-
-export const orderByPopulation = (order) => {
-    return {
-        type: "ORDER_BY_POPULATION",
-        payload: order
-    }
-}
-
-// pagination
-export const setPaginationParams = (parameters) => {
-    return {
-        type: "SET_PAGINATION",
-        payload: parameters
+        type: "SET_THEME",
+        payload: theme
     }
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,98 +1,28 @@
 
 const initialState = {
-    countries: [],
-    country: {},
-    activities: [],
-    activity: {},
-    continents: [],
-    filterState: {},
-    pagination_params: {},
+  comics: [],
+  comic: {},
+  theme: "light"
+};
+
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_ALL_COMICS":
+      return {
+        ...state,
+        comics: action.payload
+      }
+
+    case "SET_THEME":
+      return {
+        ...state,
+        theme: action.payload,
+      }
+
+
+    default: return state
   };
-  
-  
-  const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case "GET_ALL_COUNTRIES":
-        return {
-          ...state,
-          countries: action.payload
-        }
-      case "GET_COUNTRIES_BYNAME":
-        return {
-          ...state,
-          countries: action.payload
-        }
-  
-      case "GET_COUNTRY_DETAIL":
-        return {
-          ...state,
-          country: action.payload
-        }
-  
-      case "GET_ALL_CONTINENTS":
-        return {
-          ...state,
-          continents: action.payload
-        }
-  
-      // activities 
-      case "GET_ALL_ACTIVITIES":
-        return {
-          ...state,
-          activities: action.payload
-        }
-  
-      case "POST_ACTIVITY":
-        return {
-          ...state,
-          activity: action.payload
-        }
-  
-  
-      // filters
-      case "ORDER_BY_CONTINENTS":
-        return {
-          ...state,
-          countries: action.payload,
-        }
-      case "ORDER_BY_ACTIVITY":
-        return {
-          ...state,
-          countries: action.payload,
-        }
-      case "ORDER_BY_NAME":
-        return {
-          ...state,
-          filterState: action.payload,
-        }
-      case "ORDER_BY_POPULATION":
-        return {
-          ...state,
-          filterState: action.payload,
-        }
-  
-  
-      // pagination 
-  
-      case "SET_PAGINATION":
-        return {
-          ...state,
-          pagination_params: action.payload,
-        }
-  
-  
-      // case CREATE_HOUSE:
-      //   return {
-      //     ...state,
-      //     houses: [...state.houses, action.payload]
-      //   }
-      // case DELETE_HOUSE:
-      //   return {
-      //     ...state,
-      //     houses: state.houses.countries(house => house.id !== action.payload)
-      //   }
-      default: return state
-    };
-  };
-  
-  export default rootReducer;
+};
+
+export default rootReducer;
