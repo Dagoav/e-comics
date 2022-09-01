75cb87664383287407e65946c68ac621e4abb816
SergioMoyano <smoyano07@hotmail.com.ar>
2022-08-31 22:29:38 -0300

    CSS changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -21,11 +21,11 @@ const Sidebar = () => {
   return (
     <div className="containerSide">
       <nav className="navbar navbar-expand-sm navbar-light bg-white border-bottom">
-        <a className="navbar-brand ml-2 font-weight-bold" href="#">FORCEPAUSED</a>
+        <a className="navbar-brand ml-2 font-weight-bold">Nuestros productos</a>
       </nav >
       <div id="mobile-filter">
         <div>
-          <h6 className="p-1 border-bottom">Characters</h6>
+          <h6>Characters</h6>
           <select>
             <option value="">All characters</option>
             {
@@ -36,41 +36,31 @@ const Sidebar = () => {
           </select>
         </div>
         <div>
-          <h6 className="p-1 border-bottom">Filter By</h6>
-          <p className="mb-2">Color</p>
-          <ul className="list-group">
-            <li className="list-group-item list-group-item-action mb-2 rounded"><a href="#">
-              <span className="fa fa-circle pr-1" id="red"></span>Red
-            </a></li>
-            <li className="list-group-item list-group-item-action mb-2 rounded"><a href="#">
-              <span className="fa fa-circle pr-1" id="teal"></span>Teal
-            </a></li>
-            <li className="list-group-item list-group-item-action mb-2 rounded"><a href="#">
-              <span className="fa fa-circle pr-1" id="blue"></span>Blue
-            </a></li>
-          </ul>
+          <h6>Characters</h6>
+          <select>
+            <option value="">All characters</option>
+            {
+              char?.map(name => (
+                <option key={name} value={name}>{name}</option>
+              ))
+            }
+          </select>
         </div>
         <div>
-          <h6>Type</h6>
-          <form className="ml-md-2">
-            <div className="form-inline border rounded p-sm-2 my-2">
-              <input type="radio" name="type" id="boring" />
-              <label for="boring" className="pl-1 pt-sm-0 pt-1">Boring</label>
-            </div>
-            <div className="form-inline border rounded p-sm-2 my-2">
-              <input type="radio" name="type" id="ugly" />
-              <label for="ugly" className="pl-1 pt-sm-0 pt-1">Ugly</label>
-            </div>
-            <div className="form-inline border rounded p-md-2 p-sm-1">
-              <input type="radio" name="type" id="notugly" />
-              <label for="notugly" className="pl-1 pt-sm-0 pt-1">Not Ugly</label>
-            </div>
-          </form>
+          <h6>Characters</h6>
+          <select>
+            <option value="">All characters</option>
+            {
+              char?.map(name => (
+                <option key={name} value={name}>{name}</option>
+              ))
+            }
+          </select>
         </div>
       </div>
       <section id="sidebar">
         <div>
-          <h6 className="p-1 border-bottom">Characters</h6>
+          <h6 className="publisher">Characters</h6>
           <select>
             <option value="">All characters</option>
             {
@@ -81,36 +71,26 @@ const Sidebar = () => {
           </select>
         </div>
         <div>
-          <h6 className="p-1 border-bottom">Filter By</h6>
-          <p className="mb-2">Publisher</p>
-          <ul className="list-group">
-            <li className="list-group-item list-group-item-action mb-2 rounded"><a href="#">
-              <span className="fa fa-circle pr-1" id="red"></span>Marvel
-            </a></li>
-            <li className="list-group-item list-group-item-action mb-2 rounded"><a href="#">
-              <span className="fa fa-circle pr-1" id="teal"></span>DC Comics
-            </a></li>
-            <li className="list-group-item list-group-item-action mb-2 rounded"><a href="#">
-              <span className="fa fa-circle pr-1" id="blue"></span>Comic Vine
-            </a></li>
-          </ul>
+          <h6 className="publisher">Publishers</h6>
+          <select>
+            <option value="">All characters</option>
+            {
+              char?.map(name => (
+                <option key={name} value={name}>{name}</option>
+              ))
+            }
+          </select>
         </div>
         <div>
-          <h6>Type</h6>
-          <form className="ml-md-2">
-            <div className="form-inline border rounded p-sm-2 my-2">
-              <input type="radio" name="type" id="boring" />
-              <label for="boring" className="pl-1 pt-sm-0 pt-1">Boring</label>
-            </div>
-            <div className="form-inline border rounded p-sm-2 my-2">
-              <input type="radio" name="type" id="ugly" />
-              <label for="ugly" className="pl-1 pt-sm-0 pt-1">Ugly</label>
-            </div>
-            <div className="form-inline border rounded p-md-2 p-sm-1">
-              <input type="radio" name="type" id="notugly" />
-              <label for="notugly" className="pl-1 pt-sm-0 pt-1">Not Ugly</label>
-            </div>
-          </form>
+          <h6 className="publisher">Concepts</h6>
+          <select className="select">
+            <option value="">All characters</option>
+            {
+              char?.map(name => (
+                <option key={name} value={name}>{name}</option>
+              ))
+            }
+          </select>
         </div>
       </section>
     </div>
