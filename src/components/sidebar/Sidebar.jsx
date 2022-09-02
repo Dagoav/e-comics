972597f30a2b2fe379200f200970d730d89730c7
SergioMoyano <smoyano07@hotmail.com.ar>
2022-09-02 20:17:55 -0300

    Sidebar responsive fix

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -19,44 +19,6 @@ const Sidebar = () => {
 
   return (
     <div className="containerSide">
-      {/* <nav className="navbar navbar-expand-sm navbar-light bg-white border-bottom">
-        <a className="navbar-brand ml-2 font-weight-bold">Products</a>
-      </nav > */}
-      <div id="mobile-filter">
-        <div>
-          <h6>Characters</h6>
-          <select className="select">
-            <option value="">All characters</option>
-            {
-              char?.map(name => (
-                <option key={name} value={name}>{name}</option>
-              ))
-            }
-          </select>
-        </div>
-        <div>
-          <h6>Characters</h6>
-          <select className="select">
-            <option value="">All characters</option>
-            {
-              char?.map(name => (
-                <option key={name} value={name}>{name}</option>
-              ))
-            }
-          </select>
-        </div>
-        <div>
-          <h6>Characters</h6>
-          <select className="select">
-            <option value="">All characters</option>
-            {
-              char?.map(name => (
-                <option key={name} value={name}>{name}</option>
-              ))
-            }
-          </select>
-        </div>
-      </div>
       <section id="sidebar">
         <div>
           <h6 className="publisher">Characters</h6>
