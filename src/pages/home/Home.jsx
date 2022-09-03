07f3eb5a8ade48e7511911fa02449d83124648a8
SergioMoyano <smoyano07@hotmail.com.ar>
2022-09-03 12:44:08 -0300

    Pagination ok

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar2 from "../../components/navBar/Navbar2";
 import Carrousel2 from "../../components/carrousel/Carrousel2";
 import Sidebar from "../../components/sidebar/Sidebar";
 import ComicCard from "../../components/card/Card";
+import Paginado from '../../components/paginado/paginado.jsx'
 
 import { Col, Row } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
@@ -12,54 +13,76 @@ import Button from 'react-bootstrap/Button';
 import "./Home.css"
 
 const Home = () => {
-    const dispatch = useDispatch();
-    let comics = useSelector((state) => state.comics);
-    const [countProducts, setCountProducts] = useState(0)
+  const dispatch = useDispatch();
+  let comics = useSelector((state) => state.comics);
+  const [countProducts, setCountProducts] = useState(0)
 
-    useEffect(() => {
-        dispatch(setShoppingCart(countProducts))
-        dispatch(getAllVolumes())
-    }, [countProducts])
+  let allComics = useSelector(state => state.comics)
+
+  let [currentPage, setCurrentPage] = useState(1);
+  let [comicPerPage, setPokemonsPerPage] = useState(12)
+  let indexOfLastComic = currentPage * comicPerPage;
+  let indexOfFirstComic = indexOfLastComic - comicPerPage;
+  let currentComic = allComics.slice(indexOfFirstComic, indexOfLastComic)
+
+  const paginado = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [allComics.length, setCurrentPage]);
+
+  useEffect(() => {
+    dispatch(setShoppingCart(countProducts))
+    dispatch(getAllVolumes())
+  }, [countProducts])
 
 
-    let handleShopping = () => {
-        setCountProducts(() => countProducts + 1)
-    }
+  let handleShopping = () => {
+    setCountProducts(() => countProducts + 1)
+  }
 
 
-    return (
-        <div className="wrapper">
-            <div className="row">
-                <Navbar2 searchbar={true} />
-            </div>
-            <main className="main">
-                <Row>
-                    <Carrousel2 />
-                </Row>
-                <Row className="mt-5">
-                    <Col md={2} className="container" >
-                        <Sidebar />
-                    </Col>
-                    <Col md={9} className="container" >
-                        {
-                            comics.map(c => (
-                                <ComicCard key={c.id} data={c} />
-                            ))
-                        }
-                    </Col>
+  return (
+    <div className="wrapper">
+      <div className="row">
+        <Navbar2 searchbar={true} />
+      </div>
+      <main className="main">
+        <Row>
+          <Carrousel2 />
+        </Row>
+        <Row className="mt-5">
+          <Paginado
+            comicPerPage={comicPerPage}
+            allComics={allComics.length}
+            paginado={paginado}
+            page={currentPage}
+          />
+          <Col md={2} className="container" >
+            <Sidebar />
+          </Col>
+          <Col md={9} className="container" >
+            {
+              currentComic.map(c => (
+                <ComicCard key={c.id} data={c} />
+              ))
+            }
+          </Col>
 
-                </Row>
+        </Row>
 
-                <Button variant="success" onClick={handleShopping}>Comprar</Button>
-                <Button className='ms-2' variant="danger" onClick={() => setCountProducts(0)}>reset</Button>
-            </main>
-            <div className="row">
-                <div className="col">
-                    <footer className="footer">footer</footer>
-                </div>
-            </div>
+        <Button variant="success" onClick={handleShopping}>Comprar</Button>
+        <Button className='ms-2' variant="danger" onClick={() => setCountProducts(0)}>reset</Button>
+      </main>
+      <div className="row">
+        <div className="col">
+          <footer className="footer">footer</footer>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Home;
