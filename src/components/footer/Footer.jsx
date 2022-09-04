57922d348b59d7486a9105f9000a3afefa7329e3
SergioMoyano <smoyano07@hotmail.com.ar>
2022-09-04 14:36:29 -0300

    Responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./footer.css"
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
+import Sergio from '../../assets/SergioFoto.jpeg'
+import Logo from '../../assets/LogoRed2.png'
 
 
 const Footer = () => {
   return (
-    <div className="mt-5">
+    <div className="mt-5 containerFooter">
       <footer
         className="text-center text-lg-start text-white"
-        style={{ backgroundColor: "#000000" }}
+        style={{ backgroundColor: "#000000", height: "20" }}
       >
 
         <div className="container p-4 pb-0">
@@ -18,20 +20,21 @@ const Footer = () => {
             <div className="row">
 
               <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                <h6 className="text-uppercase mb-4 font-weight-bold">
+                <h5 className="text-uppercase mb-4 font-weight-bold">
                   E- Comics
-                </h6>
+                </h5>
                 <p>
                   Proyecto grupal final del bootcamp Soy Henry
                 </p>
+                <img src={Logo} alt="logo" width={270} height={180} className="mt-5" />
               </div>
 
-              <div className="col-md-7 col-lg-7 col-xl-7 mx-auto mt-3">
-                <h6 className="text-uppercase mb-4 font-weight-bold">Developers Team</h6>
+              <div className="col-md-9 col-lg-9 col-xl-9 mx-auto mt-3">
+                <h5 className="text-uppercase mb-4 font-weight-bold">Developers Team</h5>
                 <div className="containerDevs">
                   <div className="containerDev">
-                    <h7>Sergio Moyano</h7>
-                    <img />
+                    <h6>Daniel Gonzalez Avila</h6>
+                    <img src={Sergio} className="imgRedonda mt-3" />
                     <div className="containerIcons">
                       <a className="github" href='https://www.google.com.ar'>
                         <BsGithub />
@@ -43,8 +46,8 @@ const Footer = () => {
                   </div>
 
                   <div className="containerDev">
-                    <h7>Sergio Moyano</h7>
-                    <img />
+                    <h6>Jorge Alejandro Gonzales Firigua</h6>
+                    <img src={Sergio} className="imgRedonda mt-3" />
                     <div className="containerIcons">
                       <a className="github" href='https://www.google.com.ar'>
                         <BsGithub />
@@ -56,8 +59,8 @@ const Footer = () => {
                   </div>
 
                   <div className="containerDev">
-                    <h7>Sergio Moyano</h7>
-                    <img />
+                    <h6>Juan Carlos Ramirez Pinilla</h6>
+                    <img src={Sergio} className="imgRedonda mt-3" />
                     <div className="containerIcons">
                       <a className="github" href='https://www.google.com.ar'>
                         <BsGithub />
@@ -69,8 +72,8 @@ const Footer = () => {
                   </div>
 
                   <div className="containerDev">
-                    <h7>Sergio Moyano</h7>
-                    <img />
+                    <h6>Antonella Sol Román</h6>
+                    <img src={Sergio} className="imgRedonda mt-3" />
                     <div className="containerIcons">
                       <a className="github" href='https://www.google.com.ar'>
                         <BsGithub />
@@ -82,8 +85,8 @@ const Footer = () => {
                   </div>
 
                   <div className="containerDev">
-                    <h7>Sergio Moyano</h7>
-                    <img />
+                    <h6>Johan Stiven Turmeque</h6>
+                    <img src={Sergio} className="imgRedonda mt-3" />
                     <div className="containerIcons">
                       <a className="github" href='https://www.google.com.ar'>
                         <BsGithub />
@@ -95,8 +98,8 @@ const Footer = () => {
                   </div>
 
                   <div className="containerDev">
-                    <h7>Sergio Moyano</h7>
-                    <img />
+                    <h6>Sergio Daniel Moyano</h6>
+                    <img src={Sergio} className="imgRedonda mt-3" />
                     <div className="containerIcons">
                       <a className="github" href='https://www.google.com.ar'>
                         <BsGithub />
@@ -108,8 +111,8 @@ const Footer = () => {
                   </div>
 
                   <div className="containerDev">
-                    <h7>Sergio Moyano</h7>
-                    <img />
+                    <h6>Nathalia Setefania Beleño</h6>
+                    <img src={Sergio} className="imgRedonda mt-3" />
                     <div className="containerIcons">
                       <a className="github" href='https://www.google.com.ar'>
                         <BsGithub />
@@ -141,5 +144,6 @@ const Footer = () => {
     </div>
   )
 }
+
 
 export default Footer
