042086404e513b1ead82b08df96caada513baa36
dagoav <dagoav89@gmail.com>
2022-09-13 23:58:26 -0500

    reolve Login.css

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -6,7 +6,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
 import { Button } from 'react-bootstrap';
 import Swal from 'sweetalert2'
-import './login.css'
+import './Login.css'
 import {useAuthContext} from '../../context/authContext'
 
 const backendURL = process.env.REACT_APP_API;
